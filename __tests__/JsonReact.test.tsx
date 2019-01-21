402544328a5bb1b2d7cd087da216aec8c8d24582
guoshencheng <648772021@qq.com>
2019-01-21 21:44:06 +0800

    add custom component test

--- __tests__/JsonReact.test.tsx
+++ __tests__/JsonReact.test.tsx
@@ -2,6 +2,12 @@ import JsonReact from '../src';
 import * as ReactDOMServer from 'react-dom/server';
 import * as trenderer from 'react-test-renderer';
 
+import * as A from '../examples/components/A';
+import * as B from '../examples/components/B';
+
+JsonReact.RegisterComponent(A);
+JsonReact.RegisterComponent(B);
+
 describe('success render with json', () => {
   it('success render null as null', () => {
     const renderer = new JsonReact();
@@ -32,4 +38,37 @@ describe('success render with json', () => {
     const tree = trenderer.create(el).toJSON();
     expect(tree).toMatchSnapshot();
   }) 
+})
+
+describe('render custom component', () => {
+  const renderer = new JsonReact();
+  const el = renderer.render([{
+    type: 'div',
+    props: { style: { left: 20 } },
+    children: [
+      'WelCome ',
+      'To ',
+      'JsonReact'
+    ]
+  }, {
+    type: 'A',
+    props: { style: { color: 'white', background: 'red' } },
+    events: [{
+      event: A.eventKeys.ACLICK.key,
+      reducer: B.actionKeys.BCLICKED.key,
+    }]
+  }, {
+    type: 'B',
+  }]);
+  it('success render custom component', () => {
+    const tree = trenderer.create(el).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+  it('change rendere result when dispatch event', () => {
+    const tree = trenderer.create(el).toJSON();
+    renderer.store.dispatch({
+      type: B.actionKeys.BCLICKED.key
+    })
+    expect(tree).toMatchSnapshot();
+  })
 })
